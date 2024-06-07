a7de5bacbc854be7e1917692abc601772a03b974
Anish Sapkota <sapkotaanish000@gmail.com>
2024-06-07 10:56:48 +0545

    cleaned terraform code

--- dockerfile/terraform/main.tf
+++ dockerfile/terraform/main.tf
@@ -3,24 +3,5 @@ module "ecr" {
 
   repository_name = "docker-anish"
 
-  # repository_read_write_access_arns = ["arn:aws:iam::012345678901:role/terraform"]
   create_lifecycle_policy	= false
-  # repository_lifecycle_policy = jsonencode({
-  #   rules = [
-  #     {
-  #       rulePriority = 1,
-  #       description  = "Keep last 30 images",
-  #       selection = {
-  #         tagStatus     = "tagged",
-  #         tagPrefixList = ["v"],
-  #         countType     = "imageCountMoreThan",
-  #         countNumber   = 30
-  #       },
-  #       action = {
-  #         type = "expire"
-  #       }
-  #     }
-  #   ]
-  # })
-
 }
